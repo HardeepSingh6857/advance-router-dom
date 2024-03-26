0d6213d706774611644d297df25b4067d6091124
Hardeep Singh <iharry6857@gmail.com>
2024-03-26 13:39:09 +0530

    Added actions and form validation plus new pages and its module.css.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
-// Challenge / Exercise
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Root";
-import Error from "./pages/Error";
-import HomePage from "./pages/HomePage";
-import Events, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEventPage";
-import EditEventPage from "./pages/EditEvent";
-import EventsRoot from "./pages/EventsRoot";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/HomePage';
+import NewEventPage from './pages/NewEventPage';
+import RootLayout from './pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />,
-    errorElement: <Error />,
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events', element: <EventsRoot />,
+        path: 'events',
+        element: <EventsRootLayout />,
         children: [
           {
-            index: true, element: <Events />,
+            index: true,
+            element: <EventsPage />,
             loader: eventsLoader,
           },
           {
@@ -32,18 +38,32 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
-              { path: 'edit', element: <EditEventPage /> },
-            ]
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
-        ]
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
 ]);
 
-const App = () => {
+function App() {
   return <RouterProvider router={router} />;
 }
 
--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { json, useRouteLoaderData } from 'react-router-dom';
+import { json, redirect, useRouteLoaderData } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 
 const EventDetailPage = () => {
@@ -7,7 +7,6 @@ const EventDetailPage = () => {
     return (
         <>
             <EventItem event={data.event} />
-
         </>
     )
 }
@@ -16,6 +15,7 @@ export default EventDetailPage
 
 export async function loader({ request, params }) {
     const id = params.eventId;
+
     const response = await fetch('http://localhost:8080/events/' + id);
 
     if (!response.ok) {
@@ -23,4 +23,18 @@ export async function loader({ request, params }) {
     } else {
         return response
     }
+}
+
+export async function action({ params, request }) {
+    const eventId = params.eventId;
+
+    const response = await fetch('http://localhost:8080/events/' + eventId, {
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        return json({ message: 'Could not delete event.' }, { status: 500 })
+    }
+
+    return redirect('/events');
 }
